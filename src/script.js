63c9413f67d47ad68409b7b19cf143f370787062
katixyz <139153535+katixyz@users.noreply.github.com>
2024-02-04 19:43:11 +0100

    Added the functionality for the local time and improved display of the city names

--- src/script.js
+++ src/script.js
@@ -1,18 +1,36 @@
 function showTime(event) {
   if (event.target.value.length > 0) {
-    let currentTime = moment().tz(event.target.value);
+    let cityTimezone = event.target.value;
+    let cityName = cityTimezone.replace("_", " ").split("/")[1];
+
+    let currentTime = moment().tz(cityTimezone);
 
     let date = document.querySelector("#selected-city .date");
     date.innerHTML = currentTime.format("dddd, Do MMMM, YYYY");
     let time = document.querySelector("#selected-city .time");
     time.innerHTML = currentTime.format("h:mm:ss [<small>]a[</small>]");
     let city = document.querySelector("#timezone");
-    city.innerHTML = currentTime._z.name;
-    console.log(currentTime);
+    city.innerHTML = cityName;
   }
 }
 
-// setInterval(showTime, 1000);
-
 let citiesSelect = document.querySelector("#cities");
 citiesSelect.addEventListener("change", showTime);
+
+// setInterval(showTime, 1000);
+
+function showLocalTime() {
+  let localTimezone = moment.tz.guess();
+  let localCity = localTimezone.replace("_", " ").split("/")[1];
+  let localTimezoneElement = document.querySelector("#localTimezone");
+  localTimezoneElement.innerHTML = localCity;
+  let localDate = document.querySelector("#localDate");
+  localDate.innerHTML = moment.tz(localTimezone).format("dddd, Do MMMM, YYYY");
+  let localTime = document.querySelector("#localTime");
+  localTime.innerHTML = moment
+    .tz(localTimezone)
+    .format("h:mm:ss [<small>]a[</small>]");
+}
+
+showLocalTime();
+setInterval(showLocalTime, 1000);
