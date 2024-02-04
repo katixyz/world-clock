1cc22b7c501ae07242a4135c64cdf08976bac327
katixyz <139153535+katixyz@users.noreply.github.com>
2024-02-04 19:08:32 +0100

    Added the additional cities of the select dropdown

--- src/script.js
+++ src/script.js
@@ -1,17 +1,17 @@
 function showTime(event) {
   if (event.target.value.length > 0) {
-    if (event.target.value === "Europe/London") {
-      let currentTime = moment().tz(event.target.value);
+    let currentTime = moment().tz(event.target.value);
 
-      let date = document.querySelector("#selected-city .date");
-      date.innerHTML = currentTime.format("dddd, Do MMMM, YYYY");
-      let time = document.querySelector("#selected-city .time");
-      time.innerHTML = currentTime.format("h:mm:ss [<small>]a[</small>]");
-      let city = document.querySelector("#timezone");
-      city.innerHTML = "London";
-    }
+    let date = document.querySelector("#selected-city .date");
+    date.innerHTML = currentTime.format("dddd, Do MMMM, YYYY");
+    let time = document.querySelector("#selected-city .time");
+    time.innerHTML = currentTime.format("h:mm:ss [<small>]a[</small>]");
+    let city = document.querySelector("#timezone");
+    city.innerHTML = currentTime._z.name;
+    console.log(currentTime);
   }
 }
+
 // setInterval(showTime, 1000);
 
 let citiesSelect = document.querySelector("#cities");
